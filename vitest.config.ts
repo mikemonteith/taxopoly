a267ef79a39b7f0c8ade6f520f8acc4a9bfd9fdc
Mike Monteith <mike@mikemonteith.com>
2026-07-17 16:53:41 +0100

    Add ~ alias for file imports

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,13 @@
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
+  },
+  resolve: {
+    alias: {
+      "~": fileURLToPath(new URL("./app", import.meta.url)),
+    },
   },
 });
